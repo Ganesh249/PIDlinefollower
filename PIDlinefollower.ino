282b70a37a387c954d64df2dcd96bbc2ad1d48ea
Ganesh249 <76803027+Ganesh249@users.noreply.github.com>
2024-06-21 13:10:24 +0530

    ver1.03

--- PIDlinefollower.ino
+++ PIDlinefollower.ino
@@ -1,14 +1,25 @@
+
 //leftmotor
-const int en1 = 9;
-const int lmp = 2;
-const int lmn = 3;
+#define en1 9
+#define lmp 2
+#define lmn 3
 
 //Rightmotor
-const int en2 = 10;
-const int rmp = 5;
-const int rmn = 4;
+#define en2 10
+#define rmp 5
+#define rmn 4
 
-const int maxspeed = 255;
+int P, D, I, previousError, PIDvalue, error;
+int lsp, rsp;
+int lfspeed = 255;
+
+float Kp = 0;
+float Kd = 0;
+float Ki = 0 ;
+
+int minValues[6], maxValues[6], threshold[6];
+
+void calibrate();
 
 void setup() {
   pinMode(lmp,OUTPUT);
@@ -19,38 +30,70 @@ void setup() {
   pinMode(rmp,OUTPUT);
   pinMode(en2,OUTPUT);
 
+  pinMode(11,INPUT_PULLUP);
+  pinMode(12,INPUT_PULLUP);
+
   Serial.begin(9600);
+}
+void loop(){
+    while(digitalRead(11)) {}
+    Serial.println("Calibrating");
+    delay(1000);
+    calibrate();
 
 }
+void drivemotor(int motor, int speed)
+{
+  int pin1, pin2;
 
-void loop(){
-  for (int i = 0;i<10;i++){
-    Serial.println("forward");
-    analogWrite(en1,maxspeed);
-    analogWrite(en2,maxspeed);
-    forward();
-    
+  if (motor == 1) {
+    pin1 = lmp;
+    pin2 = lmn;
+    analogWrite(en1, speed);
+  } else {
+    pin1 = rmp;
+    pin2 = rmn;
+    analogWrite(en2, speed);
+  }
 
-    delay(3000);
-
-    Serial.println("Backward");
-    analogWrite(en1,maxspeed);
-    analogWrite(en2,maxspeed);
-    backward();
-    delay(3000);
-    
+  if (speed > 0) {
+    digitalWrite(pin1, HIGH);
+    digitalWrite(pin2, LOW);
+  } else if (speed < 0) {
+    digitalWrite(pin1, LOW);
+    digitalWrite(pin2, HIGH);
+  } else {
+    digitalWrite(pin1, LOW);
+    digitalWrite(pin2, LOW);
   }
 }
+void calibrate(){
+  drivemotor(1,150);
+  drivemotor(2,-150);
+  for (int i = 0;i<5;i++){
+    minValues[i] = analogRead(i);
+    maxValues[i] = analogRead(i);
+  }
+  for(int i = 0;i<3000;i++){
+    for(int j = 0;j<5;j++){
+      if (analogRead(j) < minValues[j])
+      {
+        minValues[j] = analogRead(j);
+      }
+      if (analogRead(j) > maxValues[j])
+      {
+        maxValues[j] = analogRead(j);
+      }
+    }
+  }
+  for ( int i = 0; i < 5; i++){
+    threshold[i] = (minValues[i] + maxValues[i]) / 2;
+    Serial.print(threshold[i]);
+    Serial.print("   ");
+  }
+  Serial.println();
 
-void forward(){
-  digitalWrite(lmp,HIGH);
-  digitalWrite(lmn,LOW);
-  digitalWrite(rmp,HIGH);
-  digitalWrite(rmn,LOW);
+  drivemotor(1,0);
+  drivemotor(2,0);
 }
-void backward(){
-  digitalWrite(lmp,LOW);
-  digitalWrite(lmn,HIGH);
-  digitalWrite(rmp,LOW);
-  digitalWrite(rmn,HIGH);
-}
+
